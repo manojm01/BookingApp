8675a29bb462ee68590349c1f7b4409e6f895f34
pranav.patil <pranav.patil@rackwareinc.com>
2023-04-24 01:09:30 +0530

    route update

--- bookingapp/src/pages/list/List.js
+++ bookingapp/src/pages/list/List.js
@@ -19,7 +19,7 @@ const List = () => {
   const [max, setMax] = useState(undefined);
 
   const { data, loading, reFetch } = useFetch(
-    `/hotels?city=${destination}&min=${min || 0 }&max=${max || 999}`
+    `https://bookmystayapi.onrender.com/api/hotels?city=${destination}&min=${min || 0 }&max=${max || 999}`
   );
 
   const handleClick = () => {
