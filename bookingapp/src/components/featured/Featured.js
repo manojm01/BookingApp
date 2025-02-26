b2a066dbf9542a61efabe21ccafb9bcd1ff6c4b0
Manoj Metgud <69072494+manojm01@users.noreply.github.com>
2025-02-26 11:18:34 +0530

    lazy loading img tag

--- bookingapp/src/components/featured/Featured.js
+++ bookingapp/src/components/featured/Featured.js
@@ -15,6 +15,7 @@ const Featured = () => {
               src="https://cf.bstatic.com/xdata/images/city/max500/957801.webp?k=a969e39bcd40cdcc21786ba92826063e3cb09bf307bcfeac2aa392b838e9b7a5&o="
               alt=""
               className="featuredImg"
+              loading="lazy" 
             />
             <div className="featuredTitles">
               <h1>Berlin</h1>
@@ -26,6 +27,7 @@ const Featured = () => {
               src="https://cf.bstatic.com/xdata/images/city/max500/957801.webp?k=a969e39bcd40cdcc21786ba92826063e3cb09bf307bcfeac2aa392b838e9b7a5&o="
               alt=""
               className="featuredImg"
+              loading="lazy" 
             />
             <div className="featuredTitles">
               <h1>Madrid</h1>
