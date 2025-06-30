d9599229d4589d4c37a475598b059d1fa873ab62
Manoj Metgud <69072494+manojm01@users.noreply.github.com>
2025-06-30 22:09:53 +0530

    minor change

--- bookingapp/src/components/featured/Featured.js
+++ bookingapp/src/components/featured/Featured.js
@@ -25,7 +25,7 @@ const Featured = () => {
           </div>
           <div className="featuredItem">
             <img
-              src="https://dynamic-media-cdn.tripadvisor.com/media/photo-o/12/99/1a/c7/barcelo-imagine.jpg?w=1800&h=1000&s=1"
+              src="https://dynamic-media-cdn.tripadvisor.com/media/photo-o/12/99/1a/c7/barcelo-imagine.jpg?w=500&h=500&s=1"
               alt=""
               style={{width:"100%"}}
               className="featuredImg"
@@ -38,7 +38,7 @@ const Featured = () => {
           </div>
           <div className="featuredItem">
             <img
-              src="https://dynamic-media-cdn.tripadvisor.com/media/photo-o/16/96/54/22/the-nadler-covent-garden.jpg?w=600&h=600&s=1"
+              src="https://dynamic-media-cdn.tripadvisor.com/media/photo-o/16/96/54/22/the-nadler-covent-garden.jpg?w=500&h=500&s=1"
               alt=""
               style={{width:"100%"}}
               className="featuredImg"
